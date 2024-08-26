8b977052662ef671c0c25ab216e33a4038a1ebd6
Sivatmika <164601667+SivatmikaVallabhaneni@users.noreply.github.com>
2024-08-26 15:00:35 +0530

    Add files via upload

---
+++ src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "1. Which of the three banks will be merged with the other two to create India's third-largest bank?",
+            "2. What is the name of the weak zone of the earth's crust?",
+            "3. Where was India's first national Museum opened?",
+            "4. In 2019, Which popular singer was awarded the Bharat Ratna award?",
+            "5. The world's nation 5G mobile network was launched by which country?",
+            "6. Which one of the following river flows between Vindhyan and Satpura ranges?",
+            "7. The Central Rice Research Station is situated in?",
+            "8. Who among the following wrote Sanskrit grammar?",
+            "9. Which among the following headstreams meets the Ganges in last?",
+            "10. The metal whose salts are sensitive to light is?",
+    };
+
+    public static String choices [][] = {
+            {"Punjab National Bank", "Indian Bank", "Bank of Baroda", "Dena Bank"},
+            {"Seismic", "Cosmic", "Formic", "Anaemic"},
+            {"Delhi", "Hyderabad", "Rajasthan", "Mumbai"},
+            {"Lata Mangeshkar", "Asha Bhosle", "Bhupen Hazarika", "Manna Dey"},
+            {"Japan", "Asia", "South Korea", "Malaysia"},
+            {"Narmada", "Mahanadi", "Son", "Netravathi"},
+            {"Chennai", "Cuttack", "Bangalore", "Quilon"},
+            {"Kalidasa", "Charak", "Panini", "Aryabhatt"},
+            {"Alaknanda", "Pindar", "Mandakini", "Bhagirathi"}
+    };
+
+    public static String correctAnswers []={
+            "Indian Bank",
+            "Seismic",
+            "Mumbai",
+            "Bhupen Hazarika",
+            "South Korea",
+            "Narmada",
+            "Cuttack",
+            "Panini",
+            "Bhagirathi"
+
+    };
+
+
+}
